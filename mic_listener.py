73a0a0d799cd87940f7d3d7605b22665e2d9b613
Android-JCPP-Servo <astewart1138@gmail.com>
2022-12-08 16:26:53 -0600

    Initialize constants

--- mic_listener.py
+++ mic_listener.py
@@ -7,3 +7,10 @@ This file is meant to handle all microphone events
 import pyaudio as pya
 import numpy as np
 
+# Initialize constants
+FRAMES_PER_BUFFER = 3200
+FORMAT = pya.paInt16
+CHANNELS = 1
+RATE = 16000
+p = pya.PyAudio()
+
