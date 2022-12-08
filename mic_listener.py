50b41554afde4422458750e267fc0a133a4008b1
Android-JCPP-Servo <astewart1138@gmail.com>
2022-12-08 16:33:59 -0600

    Create helper function for listening to/recording audio

--- mic_listener.py
+++ mic_listener.py
@@ -14,3 +14,30 @@ CHANNELS = 1
 RATE = 16000
 p = pya.PyAudio()
 
+# Helper function for listening to/recording audio
+def record_audio():
+    # Establish audio stream for listening and recording
+    stream = p.open(
+        format=FORMAT, 
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=FRAMES_PER_BUFFER
+    )
+
+    # Initialize empty frames and number of seconds, which will be used to establish number of samples within recording
+    frames = []
+    seconds = 1
+    for i in range(0, int(RATE / FRAMES_PER_BUFFER * seconds)):
+        # Establish the data for the model
+        data = stream.read(FRAMES_PER_BUFFER)
+        # Append the data to the frames array, so all samples can be tested
+        frames.append(data)
+    
+    # Stop and close stream
+    stream.stop_stream()
+    stream.close()
+
+# Terminate PyAudio I/O
+def terminate():
+    p.terminate()
